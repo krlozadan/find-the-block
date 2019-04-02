636470de69319e1f9a79b2238bdb1a5483a9d08b
Carlos Adan <krlozadan@gmail.com>
2019-04-02 10:20:19 -0600

    Added the images to the texture cache

--- js/cell.js
+++ js/cell.js
@@ -1,5 +1,14 @@
 "use strict"
 
+export const DIAMONDS = {
+    WHITE : "images/diamond_white.png",
+    RED : "images/diamond_red.png",
+    BLUE : "images/diamond_blue.png",
+    GREEN : "images/diamond_green.png",
+    YELLOW : "images/diamond_yellow.png",
+    PURPLE : "images/diamond_purple.png"
+}
+
 export class Cell {
     constructor(iconNumber) {
         this.iconNumber = iconNumber;
--- js/uiManager.js
+++ js/uiManager.js
@@ -1,18 +1,38 @@
 "use strict"
 
+import { ICONS } from "./cell.js";
+
 export class UIManager {
     constructor() {
         this.startButton = document.getElementById("start-btn");
+        this.startButton.disabled = true;
+        this.startButton.innerHTML = "Loading...";
         this.gridTable = document.getElementById("grid");
-        this.setupPixi();
+        this.setupPixiContainer();
     }
 
-    setupPixi() {
+    setupPixiContainer() {
         const pixiContainer = document.getElementById("pixi-container");
         this.pixiApp = new PIXI.Application({ width : pixiContainer.offsetWidth, height : pixiContainer.offsetHeight });
         pixiContainer.appendChild(this.pixiApp.view);
         this.pixiApp.renderer.autoResize = true;
+        this.pixiApp.renderer.backgroundColor = 0xD7DBDD;
         window.onresize = () => this.pixiApp.renderer.resize(pixiContainer.offsetWidth, pixiContainer.offsetHeight);
+        this.loadImagesToTextureCache();
+    }
+
+    loadImagesToTextureCache() {
+        PIXI.loader
+            .add(ICONS.WHITE.image)
+            .add(ICONS.RED.image)
+            .add(ICONS.BLUE.image)
+            .add(ICONS.GREEN.image)
+            .add(ICONS.YELLOW.image)
+            .add(ICONS.PURPLE.image)
+            .load(() => {
+                this.startButton.innerHTML = "Start";
+                this.startButton.disabled = false;
+            });
     }
 
     displayGrid(grid) {
